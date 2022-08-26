650f22ed439add93076edda84d19c9a6914255c1
Igee <igbagboleye@gmail.com>
2022-08-26 09:29:51 +0100

    fixed upload modal navigation bug

--- src/components/ModalUpload.js
+++ src/components/ModalUpload.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { arrowLeft } from "../assets/icons";
 import { uploadFile } from "../assets/images";
 import { toast } from "react-toastify";
 import { closeModal } from "../features/dashboardSlice";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { toggleModal } from "../features/dashboardSlice";
 
 const ModalUpload = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const upload = (e) => {
     e.preventDefault();
     toast.success("Upload Successful 🔗");
@@ -19,16 +21,30 @@ const ModalUpload = () => {
       navigate("/reconcilation");
     }, 3000);
   };
+
+  const goBack = (num) => {
+    navigate("/reconcilation/connect-account");
+    dispatch(toggleModal(num));
+  };
+
   return (
     <Wrapper>
-      <Link to="/reconcilation/connect-account" className="top">
+      <button type="button" onClick={() => goBack(2)} className="top">
         <img src={arrowLeft} alt="back arrow" />
         <p>Go Back</p>
-      </Link>
+      </button>
       <form onSubmit={upload}>
         <div className="middle">
           <img src={uploadFile} alt="upload icon" />
           <p>Click to upload PDF file</p>
+          <input
+            name="upload"
+            type="file"
+            single={true}
+            onChange={() => toast.success("file selected")}
+            className="file-input"
+            accept="application/pdf"
+          />
         </div>
         <button type="submit" className="bottom">
           <p>Submit</p>
@@ -52,6 +68,9 @@ const Wrapper = styled.div`
     gap: 8px;
     height: 32px;
     text-decoration: none;
+    background: transparent;
+    border: none;
+    cursor: pointer;
     p {
       font-weight: 400;
       font-size: 14px;
@@ -61,12 +80,10 @@ const Wrapper = styled.div`
     }
   }
   .middle {
+    position: relative;
     margin: 48px 0 50px;
     width: 432px;
     height: 138px;
-    left: 24px;
-    top: 104px;
-
     background: #fbfbfb;
     border: 1.8px dashed #c5c5c5;
     border-radius: 10px;
@@ -82,6 +99,14 @@ const Wrapper = styled.div`
       line-height: 17px;
       color: #808080;
     }
+  }
+  .file-input {
+    height: 100%;
+    border: 2px solid blue;
+    position: absolute;
+    width: 100%;
+    opacity: 0;
+    cursor: pointer;
   }
   .bottom {
     display: flex;
